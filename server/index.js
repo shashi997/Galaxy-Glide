527383a3b27849f420d33b0817bf856cd2c28104
shashi997 <9845shashi@gmail.com>
2024-09-01 16:08:24 +0530

    Connected db with env file

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
-const port = 8080
+require('dotenv').config()
+
+const mongoose = require('mongoose')
 
 app.use(express.static('public'))
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("DB is connected");
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`App listening on port ${process.env.PORT || 8000}`);
+        })
+    })
+    .catch((e) => {
+        console.log(e);
+})
 
 app.get('/', (req, res) => {
     res.send({ title: 'Galaxy Glide Game'})
 })
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
