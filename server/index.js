e0e22b997ed29726cdd32d87ed34f53840db15e6
shashi997 <9845shashi@gmail.com>
2024-10-13 21:28:52 +0530

    backend works frontend url linked

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://galaxy-glide-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, 
 }))
